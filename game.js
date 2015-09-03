d7e28b9e640c47bc388a5d45accc21f8ed22b182
quantumpotato <quantumpotato@gmail.com>
2015-09-03 13:19:24 -0400

    Log game board in console

---
+++ game.js
@@ -0,0 +1,8 @@
+var game = {};
+
+game.players = {'1': {'piece':'_', 'data':{'team':'X'}}, '2': {'piece':'_', 'data':{'team':'O'}} }
+game.board = [[' ', ' ', ' '],
+              [' ', ' ', ' '],
+              [' ', ' ', ' ']]; 
+
+console.log(game)
