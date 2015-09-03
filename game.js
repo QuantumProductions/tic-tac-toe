29532ed2184c027a3284c710a89977b4965c259a
quantumpotato <quantumpotato@gmail.com>
2015-09-03 13:26:31 -0400

    Change piece on board\!

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
 var game = {};
 
-game.players = {'1': {'piece':'_', 'data':{'team':'X'}}, '2': {'piece':'_', 'data':{'team':'O'}} }
+game.players = {'1': {'piece':'x', 'data':{'team':'X'}}, '2': {'piece':'o', 'data':{'team':'O'}} }
 game.board = [[' ', ' ', ' '],
               [' ', ' ', ' '],
               [' ', ' ', ' ']]; 
 
 game.pieces = {
-	' ': function(board, piece, x, y) {
-		console.log('x' + x);
-		console.log('y' + y);
+	' ': function(board, piece, x, y, data) {
+		if (piece === 'x') {
+			board[y][x] = "X";
+		}
 	}
 };
 
-game.pieces[game.board[0][0]](game.board, null, 3, 5);
+console.log(game.board[0][0]);
 
+game.pieces[game.board[0][0]](game.board, "x", 0, 0);
+
+console.log(game.board[0][0]);
