adac3edf98fe29c7a04883f877f4a22298fff0cd
quantumpotato <quantumpotato@gmail.com>
2015-09-04 21:56:27 -0400

    Evaluate winner

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ var move = function(player, x, y) {
 	var targetPiece = game.board[y][x];
 	var pieceFunction = game.pieces[targetPiece];
 	pieceFunction(game.board, piece, x, y, null);
+	evaluateResolution();
 	if (game.board.error != null) {
 		console.log(game.board.error)
 	} else {
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -23,10 +23,52 @@ var setupGame = function(){
 
 }
 
-cyclePlayers = function() {
+var cyclePlayers = function() {
 		if (game.current_player === "X") {
 			game.current_player = "O";
-		} else if (game.current_player === "Y") {
+		} else if (game.current_player === "O") {
 			game.current_player = "X";
 		}
+}
+
+var evaluateResolution = function() {
+	var board = game.board;
+	var winningAlignments = [
+	 	[[0,0], [0,1], [0,2]],
+	 	[[1,0], [1,1], [2,1]],
+	 	[[2,0], [2,1], [2,2]],
+	 	[[0,0], [1,1], [2,2]],
+	 	[[2,0], [1,1], [0,2]],
+	 	[[0,0], [1,0], [2,0]],
+	 	[[0,1], [1,1], [2,1]],
+		[[0,2], [1,2], [2,2]]];
+
+	var i = 0;
+	var j = 0;
+	
+	for (i = 0; i < winningAlignments.length; i++) {
+		var alignment = winningAlignments[i];
+		var xTiles = 0;
+		var oTiles = 0;
+
+		for (j = 0; j < alignment.length; j++) {
+			var position = alignment[j];
+			var tile = board[position[0]][position[1]];
+			if (tile === "X") {
+				xTiles++;
+			} else if (tile === "O") {
+				oTiles++;
+			}
+		}
+
+		if (xTiles == 3) {
+			game.winner = "X";
+			break;
+		} else if (oTiles == 3) {
+			game.winner = "O";
+			break;
+		}
+	}
+ 
+ 	console.log("winner: " + game.winner);
 }
